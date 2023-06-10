0b851a2c587093b5e2e01126b16034e62a9e28c4
John Isaac Rizon <jirizon693@gmail.com>
2023-06-10 08:03:46 +0800

    Added a code for the Seatworks Section
    I have finally added some code into the seatworks section of the program. My biggest gripe now is where to put the object of the Formulas class now and how to update an object's properties. I can sense a solution on the tip of my tongue but I'm asking for your advice on how to do it efficiently.

--- BusinessLayer/Formulas.cs
+++ BusinessLayer/Formulas.cs
@@ -4,6 +4,86 @@ namespace BusinessLayer
 {
     public class Formulas
     {
-    
+        public void ConstantItemScore(int x) //If the Written Works share the same number of items
+        {
+
+            try
+            {
+                int Constantitems;
+                int score;
+                double sum = 0.0;
+                Console.Write("What is your constant score?: ");
+                Constantitems = Convert.ToInt32(Console.ReadLine());
+                for (int i = 1; i <= x; i++)
+                {
+                    Console.Write("{0}. Written Work Score: ", i);
+                    score = Convert.ToInt32(Console.ReadLine());
+                    sum += score;
+
+                    if (score > Constantitems)
+                    {
+                        Console.WriteLine("Score cannot be higher than item limit. Returning to Main Menu");
+                        StudentInfo.studentlist.RemoveAt(studentlist.Count - 1);
+                        return;
+                    }
+
+                }
+
+                double itemall = Constantitems * x;
+                double grade = (sum / itemall) * 100;
+                Console.WriteLine($"Your grade is {grade}");
+                Grades.Add(grade);
+            }
+            catch
+            {
+                Console.WriteLine("An Invalid Input was Detected");
+            }
+
+
+        }
+
+        public void DynamicItemScore(int x) //If the Written Works have different number of items
+        {
+
+            try
+            {
+                int Dynamicitems = 0;
+                int score;
+                double sum = 0.0;
+                double scoresum = 0.0;
+
+                for (int i = 1; i <= x; i++)
+                {
+                    Console.Write("{0}. Written Work Score: ", i);
+                    score = Convert.ToInt32(Console.ReadLine());
+
+
+                    Console.Write("{0}. Score Over: ", i);
+                    Dynamicitems = Convert.ToInt32(Console.ReadLine());
+                    sum += score;
+                    scoresum += Dynamicitems;
+
+                    if (score > Dynamicitems)
+                    {
+                        Console.WriteLine("Score cannot be higher than constant item. Returning to Main Menu");
+                        StudentInfo.studentlist.RemoveAt(studentlist.Count - 1);
+                        return;
+                    }
+                }
+
+
+
+
+                double grade = (sum / scoresum) * 100;
+                Console.WriteLine($"Your grade is {grade}");
+                Grades.Add(grade);
+            }
+            catch
+            {
+                Console.WriteLine("An invalid input was detected");
+            }
+
+
+        }
     }
 }
