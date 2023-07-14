ade6cdc114be2c4dba7e88d4952e761e9dc408e2
VictorTroyAvila <116412427+VictorTroyAvila@users.noreply.github.com>
2023-07-14 16:33:19 +0800

    add db connection

---
+++ DataLayer/SQLDATABASE.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataLayer
+{
+    public class SQLDATABASE
+    {
+        string Connectionsstring;
+        static string ConnectString = "Data Source =MSI; Initial Catalog = GradingSystem; Integrated Security = True;";
+        // SqlConnection connect = new SqlConnection(ConnectString);
+
+        static SqlConnection connection;
+       
+    }
+}
