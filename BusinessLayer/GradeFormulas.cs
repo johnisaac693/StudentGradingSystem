88b48b72ad4fc2e63ad61a1d9dfc151874b9f7f8
Lilac Begino <lilac21laica@gmail.com>
2023-07-20 20:29:43 +0800

    added grade tabulator

--- BusinessLayer/GradeFormulas.cs
+++ BusinessLayer/GradeFormulas.cs
@@ -19,8 +19,22 @@ namespace BusinessLayer
             return graderound;
         }
 
+        /*public static double TotatlGradeCompute()
+        {
+            double grade = 0;
+        }*/
 
+        public static double TabulateGrade(double swgrade, double qgrade, double recitgrade, double attgrade, double projgrade, double examgrade)
+        {
+            double ClassStanding = ((swgrade + qgrade + recitgrade + attgrade + projgrade) / 5) * 100 * 0.7;
+            double ExamStanding = examgrade * 0.3;
 
+            double TabulatedGrade = ClassStanding + ExamStanding;
+            double tabulatedround = Math.Round(TabulatedGrade, 2);
+
+            return tabulatedround;
+
+        }
 
     }
 
