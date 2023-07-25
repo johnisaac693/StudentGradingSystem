5229981cb2cc7af85f9606d536e8443a109e0542
Jimwell01 <jimwellrosario@gmail.com>
2023-07-25 13:41:13 +0800

    created student side gui

--- StudentGradingSystem/MainSystem.cs
+++ StudentGradingSystem/MainSystem.cs
@@ -16,13 +16,34 @@ public class MainSystem
     static void Main()
     {
         UserInterfaces UI = new UserInterfaces();
+        StudentGUI studui = new StudentGUI();
+        GradesDataService gradedb = new GradesDataService();
         if (ProfessorInfo.FacultyCheck())
         {
             UI.MainMenu();
         }
         else 
         {
-            Console.WriteLine("Sorry, you are not allowed to access this page");
+            string nameexist = studui.Login();
+
+            if (UI.IsStudentExisting(nameexist))
+            {
+                var gradingdata = gradedb.GetGradesByName(nameexist);
+
+                Console.WriteLine("Student Name: " + gradingdata.Studentname);
+                Console.WriteLine("Midterm Grade: " + gradingdata.Midtermgrade);
+                Console.WriteLine("Final Grade: " + gradingdata.Finalgrade);
+                Console.WriteLine("Total Grade: " + gradingdata.Totalgrade);
+
+                Console.WriteLine(" ");
+
+
+            }
+            else
+            {
+                Console.WriteLine("Student Does Not Exist!");
+            }
+
         }
     }
 
